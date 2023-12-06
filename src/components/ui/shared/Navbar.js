9068f9bbff5e3c489ab1fa8744a06aaaaa89030b
Mahfuja Ahasan Muna <mahfujaahsanmuna@gmail.com>
2023-12-06 16:57:46 +0600

    first commit

---
+++ src/components/ui/shared/Navbar.js
@@ -0,0 +1,78 @@
+"use client";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { IconButton, Stack } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import Link from "next/link";
+
+const navItems = [
+  {
+    route: "Home",
+    pathName: "/",
+  },
+  {
+    route: "News",
+    pathName: "/news",
+  },
+  {
+    route: "About",
+    pathName: "/about",
+  },
+  {
+    route: "Contact",
+    pathName: "/contact",
+  },
+];
+
+function Navbar() {
+  return (
+    <AppBar position="static" className="bg-gray-700">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Image src={logo} width={100} height={100} alt="logo" />
+          <Box className="w-full text-center">
+            {navItems.map((item) => (
+              <Link key={item} href={item.pathName}>
+                <Button className="text-white">{item.route}</Button>
+              </Link>
+            ))}
+          </Box>
+          <Box>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                "& svg": {
+                  color: "white",
+                },
+              }}
+            >
+              <IconButton>
+                <FacebookIcon></FacebookIcon>
+              </IconButton>
+              <IconButton>
+                <InstagramIcon></InstagramIcon>
+              </IconButton>
+              <IconButton>
+                <TwitterIcon></TwitterIcon>
+              </IconButton>
+              <IconButton>
+                <YouTubeIcon></YouTubeIcon>
+              </IconButton>
+            </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
